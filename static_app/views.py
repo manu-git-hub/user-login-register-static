662a39b4668ef18a51db7125ba02e4848a431c4e
Manu S <ms.manu.856@gmail.com>
2023-02-08 16:06:10 +0530

    static commit

---
+++ static_app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from .models import Place, People
+
+
+# Create your views here.
+def demo(request):
+    obj = Place.objects.all()
+    obj1 = People.objects.all()
+    return render(request, 'index.html', {'result': obj, 'names': obj1})
+
+#
+# def home(request):
+#     return render(request, 'index.html')
